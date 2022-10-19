c5dae162c673e40b93a54180e69327cc70130d9b
Emmydrm <emmypee222@gmail.com>
2022-10-19 14:41:05 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 1024, result = 0, i;
+
+	for (i = 0; i <= n; i++)
+		if (i % 3 == 0)
+		{
+			result += i;
+		}
+		else if (i % 5 == 0)
+		{
+			result += i;
+		}
+		else
+		{
+			continue;
+		}
+	printf("%d\n", result);
+	return (0);
+}
