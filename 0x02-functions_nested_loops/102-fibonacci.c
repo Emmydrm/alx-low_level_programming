478dd457b0e78d1d3160ec7dea2391eaab291298
Emmydrm <emmypee222@gmail.com>
2022-10-19 14:52:28 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - prints fibonacci numbers
+ * @n: integer
+ *
+ * Return: result
+ */
+void fibonacci(int n)
+{
+	long int f1 = 1, f2 = 2, 1, next;
+
+	if (n < 1)
+	{
+		return;
+	}
+	printf("%1d, ", f1);
+	for (i = 1; i < (n - 1); i++)
+	{
+		printf("%1d, ", f2);
+
+		next = f1 + f2;
+		f1 = f2;
+		f2 = next;
+	}
+	printf("%1d\n", f2);
+}
+/**
+ * main - entry point
+ *
+ * Return: (0) success
+ */
+int main(void)
+{
+	fibonacci(50);
+	return (0);
+}
