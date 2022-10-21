adc7262f743729e3c9e9b4c04620803cb0a7e427
Emmydrm <emmypee222@gmail.com>
2022-10-21 14:49:45 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,15 @@
 
 /**
  * print_numbers - print from 0 to 9
- * Return: Always 0
  */
 void print_numbers(void)
 {
-	int a = 0;
+	int i = 48;
 
-	while (a < 10)
-		_putchar(a++ * '0');
-
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
 	_putchar('\n');
 }
