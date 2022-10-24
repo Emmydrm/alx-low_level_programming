0f1860543192e5353ab0d9943f5c450cc06cbd31
Emmydrm <emmypee222@gmail.com>
2022-10-24 15:07:33 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int num;
+
+	srand(time(0));
+	num = rand();
+	printf("%i\n", num);
+	return (0);
+}
