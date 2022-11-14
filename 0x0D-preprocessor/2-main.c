76f0e75299b88b24a71b871fb649d55674320e16
Emmydrm <emmypee222@gmail.com>
2022-11-14 08:30:53 -0800

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the
+ * program
+ * was complied from, folllowed by new line
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	printf("Files %s\n", __FILE__);
+
+	return (0);
+}
