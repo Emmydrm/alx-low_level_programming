9f5020458e096b9e2f0ea7d719976a19d9c328c7
Emmydrm <emmypee222@gmail.com>
2022-11-16 06:20:04 -0800

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds number
+ * @a: number 1
+ * @b: number 2
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract number
+ * @a: number 1
+ * @b: number 2
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: number 1
+ * @b: number 2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide numbers
+ * @a: Number 1
+ * @b: number 2
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a /  b);
+}
+
+/**
+ * op_mod - mods number
+ * @a: number 1
+ * @b: number 2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
