764558e80b6a100ef290291f0c47cadc1381c33f
Emmydrm <emmypee222@gmail.com>
2022-12-01 10:21:55 -0800

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: binary number
+ * Return: unsigned int or NULL or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal = 2 * decimal + (b[i] - '0');
+	}
+
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: number
+ * @index: index
+ * Return: 1 if success else -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	*n = (*n | m);
+
+	return (1);
+}
