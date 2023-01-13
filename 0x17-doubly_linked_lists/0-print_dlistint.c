07f645822bfbb45491f84f4cd47c3eb37edbfce6
Emmydrm <emmypee222@gmail.com>
2023-01-13 03:00:29 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,23 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t length = 0;
+	int count;
 
-	if (h == NULL)
-		return (length);
-	while (h->next != NULL)
+
+	count = 0;
+
+	if (h ==  NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		length++;
-		printf("%d\n", h->);
+		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
-	length++;
-	printf("%d\n", h->n);
 
-	return (length);
+	return (count);
 }
