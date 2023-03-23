1b31280ad2f212d06773661a9f33008ddb24eb06
Emmydrm <emmypee222@gmail.com>
2023-03-23 08:41:12 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,17 +13,17 @@ def island_perimeter(grid):
 
     """
 
-    total = 0
+    perimeter = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i - 1][j] == 0:
-                    total += 1
-                if j == 0 or grid[i][j - 1] == 0:
-                    total += 1
-                if i == len(grid) - 1 or grid[i + 1][j] == 0:
-                    total += 1
-                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
-                    total += 1
-    return total
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == len(grid[0]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
+    return perimeter
